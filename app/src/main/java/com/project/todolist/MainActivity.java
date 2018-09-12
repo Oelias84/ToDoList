3dfdce257b9a14bd72cdbf6f744ac81e013662d5
ofirElias <o.elias84@gmail.com>
2018-09-12 10:53:38 +0300

    My Commit

--- app/src/main/java/com/project/todolist/MainActivity.java
+++ app/src/main/java/com/project/todolist/MainActivity.java
@@ -2,17 +2,34 @@ package com.project.todolist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //Vars
+    private ArrayList<String> todos = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initRecyclerView();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, todos);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
